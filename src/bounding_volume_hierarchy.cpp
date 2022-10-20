6955e1f18d450d4e9f7f753597f7fc01e0efe7b3
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-10-20 22:16:06 +0200

    Add the Node struct. Progress with the BVH constructor.

--- src/bounding_volume_hierarchy.cpp
+++ src/bounding_volume_hierarchy.cpp
@@ -1,16 +1,104 @@
 #include "bounding_volume_hierarchy.h"
 #include "draw.h"
+#include "interpolate.h"
 #include "intersect.h"
 #include "scene.h"
 #include "texture.h"
-#include "interpolate.h"
+#include <queue>
 #include <glm/glm.hpp>
 
+struct Node {
+    glm::vec3 min;
+    glm::vec3 max;
+    std::vector<std::tuple<int, int>> indexes;
+    int divisionAxis = 0; // x = 0, y = 1, z = 2
+    int level = -1;
+    bool isLeaf;
+};
 
 BoundingVolumeHierarchy::BoundingVolumeHierarchy(Scene* pScene)
     : m_pScene(pScene)
 {
-    // TODO: implement BVH construction.
+    int desiredLevel = 5;
+    std::vector<Node> createdNodes;
+    Node root;
+    createdNodes.emplace_back(root);
+    // distribute world triangles
+    for (int i = 0; i < pScene->meshes.size(); ++i) {
+        for (int j = 0; j < pScene->meshes.at(i).vertices.size(); ++j) {
+            root.indexes.push_back(std::make_tuple(i, j));
+        }
+    }
+    std::vector<std::tuple<int, int>> triangles;
+
+    std::queue<Node> toDivide;
+    toDivide.push(root);
+
+    int divisionAxis = 0;
+    while (!toDivide.empty()) {
+        Node n = toDivide.front();
+        toDivide.pop();
+
+        if (n.level >= desiredLevel || n.indexes.size() < 2) {
+            n.isLeaf = true;
+            continue;
+        }
+
+        // The node is not a leaf
+        std::vector<float> axisCoords;
+        for (auto t : n.indexes) {
+            auto coords = pScene->meshes.at(std::get<0>(t)).vertices.at(std::get<1>(t));
+            if (coords.position.x < n.min.x)
+                n.min.x = coords.position.x;
+            else if (coords.position.x > n.max.x)
+                n.max.x = coords.position.x;
+
+            if (coords.position.y < n.min.y)
+                n.min.y = coords.position.y;
+            else if (coords.position.y > n.max.y)
+                n.max.y = coords.position.y;
+
+            if (coords.position.z < n.min.z)
+                n.min.z = coords.position.z;
+            else if (coords.position.z > n.max.z)
+                n.max.z = coords.position.z;
+
+            // find division median
+            axisCoords.push_back(coords.position[n.divisionAxis*3]);
+        }
+
+        // find min max
+        std::sort(axisCoords.begin(), axisCoords.end());
+        float median = 0;
+        if (axisCoords.size() % 2 == 1)
+            median = axisCoords[(axisCoords.size() - 1) / 2];
+        else
+            median = axisCoords[axisCoords.size() / 2];
+
+        // if level not too big then we divide
+        std::vector<std::tuple<int, int>> indexesLeft;
+        std::vector<std::tuple<int, int>> indexesRight;
+        for (auto t : triangles) {
+            auto triangle = pScene->meshes.at(std::get<0>(t)).vertices.at(std::get<1>(t));
+            if (triangle.position[n.divisionAxis*3] <= median)
+                indexesLeft.push_back(t);
+            else
+                indexesRight.push_back(t);
+        }
+
+        // create new nodes
+        divisionAxis = (divisionAxis + 1) % 2;
+        Node left = { glm::vec3 {}, glm::vec3 {}, indexesLeft, divisionAxis, n.level + 1 };
+        Node right = { glm::vec3 {}, glm::vec3 {}, indexesRight, divisionAxis, n.level + 1 };
+
+        // store them in the list and update current node's index
+        toDivide.push(left);
+        toDivide.push(right);
+        n.indexes.clear();
+        n.indexes.emplace_back(std::make_tuple(createdNodes.size(), createdNodes.size() + 1));
+        createdNodes.emplace_back(left);
+        createdNodes.emplace_back(right);
+    }
 }
 
 // Return the depth of the tree that you constructed. This is used to tell the
@@ -42,7 +130,6 @@ void BoundingVolumeHierarchy::debugDrawLevel(int level)
     drawAABB(aabb, DrawMode::Filled, glm::vec3(0.05f, 1.0f, 0.05f), 0.1f);
 }
 
-
 // Use this function to visualize your leaf nodes. This is useful for debugging. The function
 // receives the leaf node to be draw (think of the ith leaf node). Draw the AABB of the leaf node and all contained triangles.
 // You can draw the triangles with different colors. NoteL leafIdx is not the index in the node vector, it is the
@@ -60,7 +147,6 @@ void BoundingVolumeHierarchy::debugDrawLeaf(int leafIdx)
 
     // once you find the leaf node, you can use the function drawTriangle (from draw.h) to draw the contained primitives
 }
-
 
 // Return true if something is hit, returns false otherwise. Only find hits if they are closer than t stored
 // in the ray and if the intersection is on the correct side of the origin (the new t >= 0). Replace the code
