149b599a96efb4f2b9b9cef2a9bda9877dc8f0c6
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-10-28 15:54:59 +0200

    Add basic glossy reflection

--- src/shading.cpp
+++ src/shading.cpp
@@ -1,7 +1,21 @@
 #include "texture.h"
 #include <cmath>
+#include <cstdlib>
 #include <glm/geometric.hpp>
+#include <random>
 #include <shading.h>
+
+#include <iostream>
+
+namespace Generator {
+    std::mt19937 mt {std::random_device {}()};
+
+    float get(float min, float max) {
+        std::uniform_real_distribution die { min, std::nextafter(max, FLT_MAX) };
+
+        return die(mt);
+    }
+}
 
 const glm::vec3 computeShading(const glm::vec3& lightPosition, const glm::vec3& lightColor, const Features& features, Ray ray, HitInfo hitInfo)
 {
@@ -27,6 +41,41 @@ const glm::vec3 computeShading(const glm::vec3& lightPosition, const glm::vec3& 
     glm::vec3 viewVec = cameraToPoint - ray.origin;
     glm::vec3 lightVec = lightPosition - cameraToPoint;
     glm::vec3 reflected = normalize(- normalize(lightVec) + 2 * (glm::dot(normalize(lightVec), hitInfo.normal)) * hitInfo.normal);
+
+    /*
+    * GLOSSY REFLECTIONS
+    */
+    if (features.extra.enableGlossyReflection){
+        float a = 1 / ((hitInfo.material.shininess) * (hitInfo.material.shininess));
+        
+        // 1. Find the orthonormal basis:
+
+        glm::vec3 w_vec = glm::normalize(reflected);
+        // u_vec is supposed to be not collinear with w: to find it
+        // we set the smallest u_vec component to 1 (2.4.6 book)
+        glm::vec3 u_vec = w_vec;
+        if (u_vec.x < u_vec.y && u_vec.x < u_vec.z)
+            u_vec.x = 1;
+        else if (u_vec.y < u_vec.x && u_vec.y < u_vec.z)
+            u_vec.y = 1;
+        else
+            u_vec.z = 1;
+        u_vec = glm::normalize(u_vec);
+
+        glm::vec3 v_vec = glm::normalize(glm::cross(w_vec, u_vec));
+
+        // 2. Create 2 random points in [0,1]:
+        float ran1 = Generator::get(0.0f, 1.0f);
+        float ran2 = Generator::get(0.0f, 1.0f);
+
+        // 3. Calculate vector coefficients:
+        float u = -a / 2 + ran1 * a;
+        float v = -a / 2 + ran2 * a;
+
+        // 4. Replace reflected with a perturbed reflected vector
+        reflected = reflected + u * u_vec + v * v_vec;
+    }
+    
     glm::vec3 specular = lightColor * hitInfo.material.ks * pow(glm::max(0.0f, glm::dot(normalize(viewVec), reflected)), hitInfo.material.shininess);
 
     return diffuse + specular;
